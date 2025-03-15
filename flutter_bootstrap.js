5d22710039022dc05eabef6e8a429328c880d32a
venkatesh-ididala <venkateshididala@gmail.com>
2025-03-15 12:33:07 +0530

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1752872008"
+    serviceWorkerVersion: "3988344387"
   }
 });
